66da37ae962028800865ee6008d8be4919db1c22
dfh4836 <dfh4836@rit.edu>
2015-05-01 16:33:54 -0400

    FIRST COMMIT

---
+++ LyfestyleApps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LyfestyleApps.Startup))]
+namespace LyfestyleApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
